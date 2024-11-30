2daf710f9123f3c8edf4d19f05af1d9101533465
EC2 Default User <ec2-user@ip-172-31-4-38.ap-northeast-2.compute.internal>
2024-11-30 12:14:40 +0000

    F

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello{
 		System.out.println("Hello, Third");
 		System.out.println("Hello, Forth");
 		System.out.println("Hello, Fifth");
+		System.out.println("Hello, Sixth");
 
 	}
 }
