e73c922d863235c7fc8d509d2f8273139cd7b7b1
EC2 Default User <ec2-user@ip-172-31-4-38.ap-northeast-2.compute.internal>
2024-11-30 12:11:41 +0000

    C

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello, First");
 		System.out.println("Hello, Second");
+		System.out.println("Hello, Third");
 
 	}
 }
